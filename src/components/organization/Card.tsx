e4a3caf460bbe8d28029dc4077325b6f7ef38d81
ilhamnrfdn <ilhamnoer@femaledaily.com>
2025-06-20 09:12:45 +0700

    feat: detail page

--- src/components/organization/Card.tsx
+++ src/components/organization/Card.tsx
@@ -4,20 +4,30 @@ const Card: React.FC<{
 	title: string;
 	img: string;
 	publish: string;
-	key: number;
-	onClick: () => void;
+	key?: number;
+	onClick?: () => void;
+	bgColor?: string;
+	txtColor?: string;
 }> = (props) => {
-	const { title, img, publish, key, onClick } = props;
+	const {
+		title,
+		img,
+		publish,
+		key,
+		onClick,
+		bgColor = "bg-white",
+		txtColor = "text-gray-950",
+	} = props;
 	const prefixUrl = "https://media.themoviedb.org/t/p/w220_and_h330_face/";
 	return (
 		<div
 			onClick={onClick}
 			key={key}
-			className="
+			className={`
 				cursor-pointer
 				max-w-xs h-120 w-60 mx-auto
-				bg-white rounded-md shadow-lg 
-				overflow-hidden flex flex-col justify-between"
+				${bgColor} rounded-md shadow-lg 
+				overflow-hidden flex flex-col justify-between`}
 		>
 			<div className="h-screen">
 				<img
@@ -31,10 +41,10 @@ const Card: React.FC<{
 			</div>
 			<div
 				style={{ padding: "1rem" }}
-				className="h-screen"
+				className={`h-screen ${txtColor}`}
 			>
-				<p className="text-lg font-bold mb-2 text-gray-950">{title}</p>
-				<p className="text-gray-600 font-medium text-xs">{publish}</p>
+				<p className="text-lg font-bold mb-2 ">{title}</p>
+				<p className="font-medium text-xs">{publish}</p>
 			</div>
 		</div>
 	);
--- src/pages/detail-page/Index.tsx
+++ src/pages/detail-page/Index.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import Card from "../../components/organization/Card";
+import Loader from "../../components/atoms/Loader";
 
 const Index = () => {
 	const { id } = useParams();
@@ -14,7 +16,45 @@ const Index = () => {
 		],
 	});
 
-	return <div className="font-bold text-black w-screen">{id}</div>;
+	return (
+		<div className="font-bold text-black w-screen">
+			{isLoading ? (
+				<div className="h-screen w-screen flex justify-center items-center text-black text-9xl">
+					<Loader />
+				</div>
+			) : (
+				<div className="header w-screen ">
+					<div className="custom-bg w-7xl text-white flex justify-self-center">
+						<Card
+							img={movieDetail?.poster_path}
+							title={movieDetail?.original_title}
+							publish={movieDetail?.release_date}
+							bgColor="bg-sky-900"
+							txtColor="text-white"
+						/>
+					</div>
+				</div>
+			)}
+
+			<style>
+				{`
+					.header {
+
+						background:linear-gradient(to bottom right, rgba(10.5, 31.5, 31.5, 0.6), rgba(10.5, 31.5, 31.5, 0.84)), url(https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movieDetail?.backdrop_path}) no-repeat center center;
+						background-size: cover;
+						height: 55vh;
+						position:relative;
+						// width:100%;
+    					background-position: top calc((50vw - 200px) - 900px) left;
+						padding: 2rem 0;
+					}
+					.custom-bg {
+
+					}
+				`}
+			</style>
+		</div>
+	);
 };
 
 export default Index;
